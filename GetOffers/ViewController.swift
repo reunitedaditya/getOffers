639b521bdf49c43ffbfe58b195b4a64c44b0f674
Aditya <aditya.yadav@graymatrix.com>
2017-04-18 12:32:22 +0530

    No Connection added

--- GetOffers/ViewController.swift
+++ GetOffers/ViewController.swift
@@ -12,6 +12,7 @@ import UserNotifications
 import UserNotificationsUI
 import SDWebImage
 import Alamofire
+import SystemConfiguration
 
 
 class ViewController: UIViewController, CXCallObserverDelegate {
@@ -20,6 +21,12 @@ class ViewController: UIViewController, CXCallObserverDelegate {
     let requestIdentifier = "SampleRequest" //identifier is to cancel the notification request
     @IBOutlet weak var productImageView: UIImageView!
     var downloadImage : Bool = true
+    var showDisconnectNotification : Bool = false
+    var internetIsBack : Bool = false
+    
+    @IBOutlet weak var noConnectionView: UIView!
+    
+ 
 
     var timer = Timer()
     var backgroundTask = BackgroundTask()
@@ -36,7 +43,7 @@ class ViewController: UIViewController, CXCallObserverDelegate {
         self.scheduledTimerWithTimeInterval()
         
         callObserver.setDelegate(self, queue: nil)
-        
+        self.noConnectionView.isHidden = true
         backgroundTask.startBackgroundTask()
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.timerAction), userInfo: nil, repeats: true)
         
@@ -52,7 +59,29 @@ class ViewController: UIViewController, CXCallObserverDelegate {
         case .background:
             
             print("background")
+            setNetworkStatus()
             
+        case .active :
+            
+        print("Active")
+        if !isInternetAvailable() {
+            
+         self.noConnectionView.isHidden = false
+         internetIsBack = true
+            
+        } else {
+            
+            if internetIsBack{
+                internetIsBack = false
+                self.noConnectionView.isHidden = true
+                defer {
+                    self.sendDataToServer()
+                }
+                
+            }
+           
+            
+            }
             
         default:
             print("")
@@ -76,7 +105,7 @@ class ViewController: UIViewController, CXCallObserverDelegate {
         if call.hasEnded {
             
             print("Call ended from delegate")
-            self.triggerNotification()
+            self.triggerNotification(title: "Get Offer", subtitle: "Checkout This Latest Offer", body: "Tap to Open")
         }
         
         if call.isOnHold {
@@ -105,13 +134,13 @@ class ViewController: UIViewController, CXCallObserverDelegate {
     
     
 
-    func triggerNotification(){   //Handle Notification
+    func triggerNotification(title : String , subtitle : String , body : String){   //Handle Notification
         
         print("notification will be triggered in a second")
         let content = UNMutableNotificationContent()
-        content.title = "Get Offer"
-        content.subtitle = "Checkout this latest offer"
-        content.body = "Tap to open"
+        content.title = title         //"Get Offer"
+        content.subtitle = subtitle          //"Checkout this latest offer"
+        content.body =  body                  //"Tap to open"
         content.sound = UNNotificationSound.default()
         
         // Deliver the notification in one second.
@@ -172,6 +201,53 @@ class ViewController: UIViewController, CXCallObserverDelegate {
         backgroundTask.stopBackgroundTask()
         backgroundTask.startBackgroundTask()
     }
+
+
+    func isInternetAvailable() -> Bool
+    {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
+    }
+
+    
+    
+    func setNetworkStatus(){
+        
+        let status = isInternetAvailable()
+        
+        if !status {
+            
+            print("Player Disconnected")
+            if !showDisconnectNotification {
+                
+                self.triggerNotification(title: "No Internet Connection", subtitle: "Please Check Your internet Connection", body: "Tap to Open")
+                showDisconnectNotification = true
+            }
+           
+
+            
+        } else {
+            
+            showDisconnectNotification = false
+            print("Player connected")
+        }
+    }
+
 }
 
 extension ViewController:UNUserNotificationCenterDelegate {
